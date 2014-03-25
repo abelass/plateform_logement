7c5815f71f377743e28dd03bc50fa0cd16322aab
Rainer <rainer@websimple.be>
2014-03-25 18:29:46 +0100

    import initial

---
+++ lang/paquet-plateform_logement_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plateform_logement_description' => 'Thème et configuration du site',
+	'plateform_logement_nom' => 'Plateforme Logement',
+	'plateform_logement_slogan' => '',
+);
+
+?>
