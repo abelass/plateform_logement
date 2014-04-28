1bc20c717ccae768119f0678d71ce2d80f288847
Rainer <rainer@websimple.be>
2014-04-28 18:31:01 +0200

    modif form petitions

---
+++ lang/local_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'form_pet_nom_site2' => 'Vous souhaitez être informé(e) de nos actions ? Indiquez votre numéro de GSM.',
+);
+
+?>
---
+++ lang/local_nl.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'form_pet_nom_site2' => 'Wil u per sms op de hoogte blijven van onze acties?  Noteer dan ook uw gsm-nummer.',
+);
+
+?>
