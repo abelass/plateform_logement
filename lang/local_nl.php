0c5ec3a20dc22e5c916c8e7046fd570e2092f428
unknown <mullrai@D02DI1146441.net1.cec.eu.int>
2014-05-16 16:39:12 +0200

    pas nécessaire, mais bon

--- lang/local_nl.php
+++ lang/local_nl.php
@@ -5,6 +5,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	//S
+	'signatures_papier'=>'Aantal ondertekenaars (web):',
+
 	// P
 	'form_pet_nom_site2' => 'Wil u per sms op de hoogte blijven van onze acties?  Noteer dan ook uw gsm-nummer.',
 );
